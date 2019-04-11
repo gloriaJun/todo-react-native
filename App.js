5739f2c6fce86581e7432a5f78b7855d43c640e3
gloriaJun <pureainu@gmail.com>
2019-04-12 01:27:19 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle={"light-content"} />
+        <Text style={styles.title}>ToDo</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"input new todo"}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F23657',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: '200',
+    marginTop: 50,
+    marginBottom: 30,
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0,
+        },
+      },
+      android: {
+        evaluate: 3,
+      },
+    })
+  },
+  input: {
+
+  },
+});
